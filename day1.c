db55d0ce1fc3796019a9b79d079934df74203086
priyanshu joshi <priyanshujoshi828@gmail.com>
2026-09-15 19:40:09 +0530

    Add files via upload

---
+++ day1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int first = -__INT_MAX__, second = -__INT_MAX__;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > first) {
+            second = first;
+            first = arr[i];
+        } else if (arr[i] > second && arr[i] != first) {
+            second = arr[i];
+        }
+    }
+
+    printf("%d\n", second);
+    return 0;
+}
